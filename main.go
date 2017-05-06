e7d3005a17726582994a6d01f8a4a0444e3dd2ef
kolonse <867594819@qq.com>
2017-05-06 09:52:51 +0800

    优化生成唯一码长度

--- main.go
+++ main.go
@@ -21,17 +21,15 @@ var storeDir = flag.String("d", "/tmp/simplekv", "-d=<dir> 活码内容存储目
 var storeName = flag.String("n", "qrcode", "-n=<name> 活码存储目录名字")
 var activeUrl = flag.String("a", "http://qrcode.youdianle.com.cn/active", "-a=<url> 连接性活码地址url")
 
-type Shortest struct {
-	Redirect bool
-	Content  string
-}
-
-var kv *simplekv.SKV
-
 func QRCode(req *HttpLib.Request, res *HttpLib.Response, next Type.Next) {
 	defer req.Body.Close()
 	s := req.URL.Query().Get("shortest")
 	query := req.URL.Query()
+	if len(query.Get("content")) == 0 {
+		http.NotFound(res.ResponseWriter, req.Request)
+		KolonseWeb.BeeLogger.Error("%s %s %s", req.RemoteAddr, req.URL.String(), "不支持空内容二维码请求")
+		return
+	}
 	if s == "true" { // 是否使用活码
 		s = query.Get("redirect")
 		st := Shortest{
@@ -45,20 +43,19 @@ func QRCode(req *HttpLib.Request, res *HttpLib.Response, next Type.Next) {
 		ctr.Write([]byte(st.Content))
 		rs := hex.EncodeToString(ctr.Sum(nil))
 		if !kv.Exist(rs) {
+			st.Index = makeUnique()
 			jbuf, _ := json.Marshal(st)
 			kv.Write(rs, jbuf)
+			kvIndex2MD5.Write(st.Index, []byte(rs))
+		} else {
+			obj := kv.Read(rs).ToJsonObject()
+			st.Index = obj["Index"].(string)
 		}
-		query.Set("content", *activeUrl+"?k="+rs)
+		query.Set("content", *activeUrl+"?k="+st.Index)
 	}
 
 	var arg qdiy.QRArg
 	arg.Parse(query)
-	if len(arg.Content) == 0 {
-		http.NotFound(res.ResponseWriter, req.Request)
-		KolonseWeb.BeeLogger.Error("%s %s %s", req.RemoteAddr, req.URL.String(), "不支持空内容二维码请求")
-		return
-	}
-
 	qlogo := qdiy.QRDiy{Arg: arg}
 	buf, err := qlogo.Encode()
 	if err != nil {
@@ -73,17 +70,22 @@ func QRCode(req *HttpLib.Request, res *HttpLib.Response, next Type.Next) {
 func Active(req *HttpLib.Request, res *HttpLib.Response, next Type.Next) {
 	defer req.Body.Close()
 	key := req.URL.Query().Get("k")
-	if !kv.Exist(key) {
+	if !kvIndex2MD5.Exist(key) {
 		http.NotFound(res.ResponseWriter, req.Request)
 		KolonseWeb.BeeLogger.Error("%s %s %s NotFound", req.RemoteAddr, req.URL.String(), key)
 		return
 	}
-	obj := kv.Read(key).ToJsonObject()
+	md5Str := kvIndex2MD5.Read(key).ToString()
+	if !kv.Exist(md5Str) {
+		http.NotFound(res.ResponseWriter, req.Request)
+		KolonseWeb.BeeLogger.Error("%s %s %s NotFound", req.RemoteAddr, req.URL.String(), key)
+		return
+	}
+	obj := kv.Read(md5Str).ToJsonObject()
 	st := Shortest{
 		Redirect: obj["Redirect"].(bool),
 		Content:  obj["Content"].(string),
 	}
-	KolonseWeb.BeeLogger.Info(key)
 	if st.Redirect { // 如果属于重定向 那么需要将请求内容返回给请求源hex
 		res.Redirect(string(st.Content))
 	} else {
@@ -98,11 +100,6 @@ func Help(req *HttpLib.Request, res *HttpLib.Response, next Type.Next) {
 }
 
 func main() {
-	var err error
-	kv, err = simplekv.NewSKV(*storeName, *storeDir)
-	if err != nil {
-		panic(err)
-	}
 	flag.Parse()
 	KolonseWeb.DefaultApp.Get("/qrcode", QRCode)
 	KolonseWeb.DefaultApp.Get("/active", Active)
@@ -111,4 +108,20 @@ func main() {
 	KolonseWeb.DefaultApp.Post("/active", Active)
 	KolonseWeb.DefaultApp.Post("/", Help)
 	KolonseWeb.DefaultApp.Listen(*ip, *port)
+}
+
+func init() {
+	var err error
+	kv, err = simplekv.NewSKV(*storeName+"/data", *storeDir)
+	if err != nil {
+		panic(err)
+	}
+	kvIndex2MD5, err = simplekv.NewSKV(*storeName+"/index", *storeDir)
+	if err != nil {
+		panic(err)
+	}
+	kvAutoIncrease, err = simplekv.NewSKV(*storeName+"/autoincrease", *storeDir)
+	if err != nil {
+		panic(err)
+	}
 }
